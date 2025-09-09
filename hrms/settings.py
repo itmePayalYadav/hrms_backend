a00d37d74bb238495b2c15b9a40562eebdb72565
tejash <tejashyadavinfo@gmail.com>
2025-09-09 21:39:49 +0530

    Update settings.py for local and production hosts

--- hrms/settings.py
+++ hrms/settings.py
@@ -12,7 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ----------------------------
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=Csv())
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1,human-resource-management.up.railway.app",
+    cast=Csv()
+)
 
 # ----------------------------
 # CUSTOM USER
