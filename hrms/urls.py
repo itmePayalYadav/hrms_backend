16221921fecb6cdc7f56d88b477844b174e4872e
tejash <tejashyadavinfo@gmail.com>
2025-09-10 18:27:16 +0530

    Add health check endpoint

--- hrms/urls.py
+++ hrms/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def health_check(request):
+    return Response({"status": "ok", "message": "Service is running"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
