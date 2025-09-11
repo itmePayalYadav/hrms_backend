9471bf2b3d705fe169849035c624810e01c4dcac
tejash <tejashyadavinfo@gmail.com>
2025-09-11 11:31:06 +0530

    Add route name 'health-check' for clarity

--- hrms/urls.py
+++ hrms/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import health_check
+from .views import HealthCheckAPIView
 
 urlpatterns = [
-    path('health/', health_check),
     path('admin/', admin.site.urls),
+    path("health/", HealthCheckAPIView.as_view(), name="health-check"),
     path("api/v1/accounts/", include(("users.urls", "users"), namespace="accounts")),  
 ]
--- hrms/views.py
+++ hrms/views.py
@@ -1,24 +1,24 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from django.utils import timezone
 from core.utils import api_response
+
 
 class HealthCheckAPIView(APIView):
     """
     Health check endpoint for monitoring service status.
     """
     permission_classes = [AllowAny]
-    
+
     def get(self, request, *args, **kwargs):
         response = {
-                "service": "HRMS API",
-                "uptime": timezone.now().isoformat(),
-                "endpoint": "health-check"
+            "service": "HRMS API",
+            "uptime": timezone.now().isoformat(),
+            "endpoint": "health-check"
         }
         return api_response(
             message="Service is running",
             data=response,
-            status_code=status.HTTP_201_CREATED
+            status_code=status.HTTP_200_OK 
         )
